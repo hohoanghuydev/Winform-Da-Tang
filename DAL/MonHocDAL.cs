f8e02121a99e00dfcf3600bbe3caf0de0ab65ba6
hohoanghuydev <hohoanghuy2603@gmail.com>
2024-10-10 21:06:50 +0700

    first lesson

---
+++ DAL/MonHocDAL.cs
@@ -0,0 +1,38 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class MonHocDAL
+    {
+        QLSINHVIEN_B7DataContext context = new QLSINHVIEN_B7DataContext();
+
+        public MonHocDAL()
+        {
+
+        }
+
+        public List<MonHoc> GetAll()
+        {
+            return context.MonHocs.Select(mh => mh).ToList<MonHoc>();
+        }
+
+        public bool Create(MonHoc monHoc)
+        {
+            try
+            {
+                context.MonHocs.InsertOnSubmit(monHoc);
+                context.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
